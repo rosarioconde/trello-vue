94365ecb2183431aa6a291152b7846a12cc68343
rosarioconde <conde.rosario88@gmail.com>
2021-06-19 20:50:14 -0300

    fetch parte de las mutaciones: request success failure, actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,7 +9,35 @@ export default {
         API.getBoardsByUser(user)
             .then(snap => commit(types.FETCH_BOARDS_SUCCESS, { boards: snap.val() }))
             .catch(error => commit(types.FETCH_BOARDS_FAILURE, { error }))
-    }
+    },
 
     //fetch via ajax de las listas asociadas a un panel
+
+    fetchLists({ commit }, { board }) {
+        commit(types.FETCH_LISTS_REQUEST)
+
+        API.getListsFromBoard()
+            .then(snap => commit(types.FETCH_LISTS_SUCCESS, { lists: snap.val() }))
+            .catch(error => commit(types.FETCH_LISTS_FAILURE, { error }))
+    },
+    //fetch via ajax de las tareas de una lista
+
+    fetchTasks({ commit }, { list }) {
+        commit(types.FETCH_TASKS_REQUEST)
+
+        API.getTasksFromList(list)
+            .then(snap => commit(types.FETCH_TASKS_SUCCESS, { tasks: snap.val() }))
+            .catch(error => commit(types.FETCH_TASKS_FAILURE, { error }))
+    },
+
+    //añadir  un nurvo panel
+    addBoard({ commit }, { name }) {
+        API.postBoard(name)
+            .then(board => commit(types.ADD_BOARD, { board }))
+    },
+
+    addColumn({ commit }, { board, name }) {
+        API.postList(board, name)
+            .then((column) => commit(types.ADD_COLUMN, { column }))
+    }
 }
